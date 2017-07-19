03e6acdab27669b9444120261bcd7179db8ced2f
axay <akshay@ourphilosophy.co.uk>
2017-07-19 13:09:55 +0530

    inital commit

---
+++ app/src/main/java/com/example/axay/volley_tutorial/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.axay.volley_tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       button=(Button)findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textView);
+        requestQueue= Volley.newRequestQueue(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://192.168.1.104/leave/public/api/users"
+                      ,null,  new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONArray jsonArray = response.getJSONArray("data");
+
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject data = jsonArray.getJSONObject(i);
+
+                                        String id = data.getString("identity");
+                                        String first_name = data.getString("first_name");
+                                        String last_name = data.getString("email");
+
+                                        textView.append( id+" "+ first_name + " " + last_name + "\n");
+
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                      @Override
+                      public void onErrorResponse(VolleyError error) {
+
+                          Log.d("error", "error from server response");
+                      }}
+
+
+                      );
+
+
+                        requestQueue.add(jsonObjectRequest);
+
+
+                  }
+
+
+        });
+    }
+}
